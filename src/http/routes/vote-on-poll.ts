fe2c5da20f735aeaf09dab3d2870e7b927f5b734
Caroline A Bandeira <carol_bandeira7@hotmail.com>
2024-02-24 08:50:14 -0300

    feat: add redis, web socket and pubsub strategy for real time voting

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import { FastifyInstance } from "fastify"
 import z from "zod"
 import { randomUUID } from "crypto"
 import { prisma } from "../../lib/prisma"
+import { redis } from "../../lib/redis"
+import { voting } from "../../utils/voting-pub-sub"
 
 export async function voteOnPoll(app: FastifyInstance) {
     app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -34,6 +36,14 @@ export async function voteOnPoll(app: FastifyInstance) {
                         id: userPreviousVoteOnPoll.id,
                     }
                 })
+                //tira a contabilização do voto
+                const votes = await redis.zincrby(pollId, -1,userPreviousVoteOnPoll.pollOptionId)
+
+                voting.publish(pollId, {
+                    pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+                    votes: Number(votes),
+                })
+
             } else if (userPreviousVoteOnPoll) {
                 return reply.status(400).send({ message: "You already voted on this poll." })
             }
@@ -59,6 +69,13 @@ export async function voteOnPoll(app: FastifyInstance) {
             }
         })
                 
+        const votes = await redis.zincrby(pollId, 1,pollOptionId)
+
+        voting.publish(pollId, {
+            pollOptionId,
+            votes: Number(votes),
+        })
+
         return reply.status(201).send()
     })
 }
--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,8 @@ import cookie from '@fastify/cookie'
 import { createdPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
+import fastifyWebsocket from '@fastify/websocket'
+import { pollResults } from './ws/poll-results'
 
 
 const app = fastify()
@@ -12,9 +14,13 @@ app.register(cookie, {
     hook: 'onRequest',
 })
 
+app.register(fastifyWebsocket)
+
 app.register(createdPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+
+app.register(pollResults)
 
 
 app.listen({ port:3333 }).then(() => {
